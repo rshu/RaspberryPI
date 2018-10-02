5a1f73bc0825ad826496d91243fd9f859372a3db
Rui Shu <terry.shu@hotmail.com>
2018-10-02 10:28:37 -0400

    update camera

--- camera/camera.py
+++ camera/camera.py
@@ -1,10 +1,29 @@
-from picamera import PiCamera
+from picamera import PiCamera, Color
 from time import sleep
 
 camera = PiCamera()
+
 camera.rotation = 180
-camera.start_preview()
+camera.resolution = (1920, 1080) # max for still photo, (2592*1944) for video
+camera.framerate = 15
+
+camera.start_preview() # preview only works when a monitor is connected, not for remote access(e.g., ssh) 
+# camera.annotate_background = Color('blue')
+# camera.annotate_foreground = Color('yellow')
+# camera.image_effect = 'colorswap'
+# camera.awb_mode = 'sunlight'
+# camera.exposure_mode = 'beach'
+camera.annotate_text = "RAISE!"
+# camera.annotate_text_size = 20
+camera.brightness = 70
+# camera.contrast = 50
+sleep(2)
 camera.capture('/home/pi/Desktop/image.jpg')
+
 camera.start_recording('haha.h264')
 sleep(5)
 camera.stop_recording()
+
+camera.stop_preview()
+
+# omxplayer haha.h264
