8fded6c355617e258a3bc4872a1ac93a93c92d5a
jianfeng <ginfungchan@gmail.com>
2018-10-10 17:02:07 -0400

    update partick exam

--- Sensors/I2C_LCD/30_i2c_lcd1602.py
+++ Sensors/I2C_LCD/30_i2c_lcd1602.py
@@ -8,20 +8,18 @@ from subprocess import *
 
 def setup():
 	LCD1602.init(0x27, 1)	# init(slave address, background light)
-	LCD1602.write(0, 0, now.strftime("%Y-%m-%d %H:%M"))
-	LCD1602.write(1, 1, 'The RAISE Lab!')
-	time.sleep(10)
+        LCD1602.clear()
 
-def loop():
+def loop(content):
 	space = '                '
-	greetings = 'Thank you for buying SunFounder Sensor Kit for Raspberry! ^_^'
+	greetings = content 
 	greetings = space + greetings
 	while True:
 		tmp = greetings
 		for i in range(0, len(greetings)):
-			LCD1602.write(0, 0, tmp)
+                        LCD1602.write(0, 2, tmp[:16])
 			tmp = tmp[1:]
-			time.sleep(0.8)
+			time.sleep(0.1)
 			LCD1602.clear()
 
 def destroy():
@@ -29,8 +27,12 @@ def destroy():
 
 
 if __name__ == "__main__":
+    """
     while True:
         LCD1602.init(0x27, 1)	# init(slave address, background light)
-        LCD1602.write(0, 0, datetime.now().strftime("%Y-%m-%d %H:%M"))
-        LCD1602.write(0, 2, 'The RAISE LAB!')
-        time.sleep(20)
+        LCD1602.write(0, 0, datetime.now().strftime("%b%d %a %H:%M"))
+        LCD1602.write(0, 2, 'Welcome Dr. Xia!')
+        time.sleep(10)
+    """
+    setup()
+    loop("Congrats for Patrick Xia passing the Written Exam!")
